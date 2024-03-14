91931402986eca9ec1f2c1db01b2316463053396
Temsaah <Seif.Temsaah@gmail.com>
2024-03-14 19:28:47 +0200

    Updated rep

---
+++ interactive-card-details-form/app.js
@@ -0,0 +1,5 @@
+let submitBtn = document.querySelector(".submit");
+let cardNumberInput = document.querySelector(".cardnum-input");
+let expDateMonth = document.querySelector(".month-input");
+let expDateYear = document.querySelector(".year-input");
+let cvcInput = document.querySelector(".cvc-input");
