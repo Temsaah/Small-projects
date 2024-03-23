2f40736bf0eb3b6c8ac51efcfc15cfc6870317b9
Temsaah <Seif.Temsaah@gmail.com>
2024-03-23 19:39:40 +0200

    Updated rep

---
+++ Intro-section-dropdown-navigation/app.js
@@ -0,0 +1,51 @@
+const menuBtn = document.querySelector(".menu");
+const mobileMenuExpand = document.querySelector(".mobile-menu-expand");
+const closeMenuBtn = document.querySelector(".close-menu");
+const overlay = document.querySelector("#overlay");
+const featureBtn = document.querySelector(".features");
+const featureMenu = document.querySelector(".feature-menu");
+const companyBtn = document.querySelector(".company");
+const companyMenu = document.querySelector(".company-menu");
+
+function hideMenu() {
+  mobileMenuExpand.classList.add("hidden");
+  overlay.classList.add("hidden");
+}
+
+function showMenu() {
+  mobileMenuExpand.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}
+
+function toggleIcon(node) {
+  const icon = node.querySelector(".toggle-icon");
+  if (icon.classList.contains("fa-chevron-down")) {
+    icon.classList.remove("fa-chevron-down");
+    icon.classList.add("fa-chevron-up");
+  } else {
+    icon.classList.remove("fa-chevron-up");
+    icon.classList.add("fa-chevron-down");
+  }
+}
+
+menuBtn.addEventListener("click", () => {
+  showMenu();
+});
+
+closeMenuBtn.addEventListener("click", () => {
+  hideMenu();
+});
+
+overlay.addEventListener("click", () => {
+  hideMenu();
+});
+
+featureBtn.addEventListener("click", () => {
+  featureMenu.classList.toggle("hidden");
+  toggleIcon(featureBtn);
+});
+
+companyBtn.addEventListener("click", () => {
+  companyMenu.classList.toggle("hidden");
+  toggleIcon(companyBtn);
+});
