a81a1a9cfc9139a62fda1d481dd5529f3290da2c
Temsaah <Seif.Temsaah@gmail.com>
2024-04-05 19:45:29 +0200

    Optimizing javascript code

--- tip-calculator-app/app.js
+++ tip-calculator-app/app.js
@@ -2,6 +2,7 @@ const billPrice = document.querySelector(".bill-result");
 const tipBtns = document.querySelectorAll(".tip-btn");
 const customTip = document.querySelector(".custom-tip");
 const peopleNumber = document.querySelector(".people-num-result");
+const resetBtn = document.querySelector(".reset-btn");
 
 const tipResult = document.querySelector(".tip-result");
 const totalResult = document.querySelector(".total-result");
@@ -10,9 +11,14 @@ let tip = null;
 let bill = null;
 let numberOfPeople = null;
 
-billPrice.addEventListener("keyup", (e) => {
-  bill = Number(billPrice.value);
-  calculateTip();
+[billPrice, customTip, peopleNumber].forEach((input) => {
+  input.addEventListener("keyup", () => {
+    if (input == customTip) {
+      tip = null;
+      toggleBtn(tipBtns);
+    }
+    calculateTip();
+  });
 });
 
 tipBtns.forEach((btn) => {
@@ -24,32 +30,45 @@ tipBtns.forEach((btn) => {
   });
 });
 
-customTip.addEventListener("keyup", () => {
-  toggleBtn(tipBtns);
-  tip = Number(customTip.value) / 100;
-  calculateTip();
+resetBtn.addEventListener("click", () => {
+  resetAll();
 });
-
-peopleNumber.addEventListener("keyup", (e) => {
-  numberOfPeople = Number(peopleNumber.value);
-  calculateTip();
-});
-
-function toggleBtn(Btns, btn = null) {
-  Btns.forEach((btn) => {
-    if (btn.classList.contains("bg-primary-strong-cyan")) {
-      btn.classList.remove("bg-primary-strong-cyan");
-    }
-  });
-
-  btn?.classList.toggle("bg-primary-strong-cyan");
-}
 
 function calculateTip() {
-  if (tip && bill && numberOfPeople) {
+  bill = Number(billPrice.value);
+  tip = tip ? tip : Number(customTip.value) / 100;
+  numberOfPeople = Number(peopleNumber.value);
+
+  if (tip >= 0 && bill >= 0 && numberOfPeople > 0) {
     let tipPerPerson = (bill * tip) / numberOfPeople;
     let billPerPerson = bill / numberOfPeople;
     tipResult.textContent = `${tipPerPerson.toFixed(2)}`;
     totalResult.textContent = `${(tipPerPerson + billPerPerson).toFixed(2)}`;
   }
+}
+
+function resetAll() {
+  bill = null;
+  tip = null;
+  numberOfPeople = null;
+
+  billPrice.value = "";
+  customTip.value = "";
+  peopleNumber.value = "";
+  toggleBtn(tipBtns);
+
+  tipResult.textContent = "0.00";
+  totalResult.textContent = "0.00";
+}
+
+function toggleBtn(Btns, btn = null) {
+  Btns.forEach((btn) => {
+    if (btn.classList.contains("bg-primary-strong-cyan")) {
+      btn.classList.remove("bg-primary-strong-cyan");
+      btn?.classList.replace("text-neutral-vdark-cyan", "text-white");
+    }
+  });
+
+  btn?.classList.toggle("bg-primary-strong-cyan");
+  btn?.classList.replace("text-white", "text-neutral-vdark-cyan");
 }
