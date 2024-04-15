255503ff3a7ea5bdea99b63037e0c044e6c98627
Temsaah <Seif.Temsaah@gmail.com>
2024-04-15 22:23:01 +0200

    Added button to remove cards

--- time-tracking-dashboard/app.js
+++ time-tracking-dashboard/app.js
@@ -3,6 +3,8 @@ const timeLapseBtns = document.querySelectorAll(".timelapse-btn");
 const dailyBtn = document.querySelector(".daily-btn");
 const weeklyBtn = document.querySelector(".weekly-btn");
 const monthlyBtn = document.querySelector(".monthly-btn");
+const ellipsisBtns = document.querySelectorAll(".ellipsis-btn");
+const deleteBtns = document.querySelector(".delete-btn");
 
 async function fetchData(timeframe) {
   try {
@@ -39,5 +41,19 @@ function addEventListenerToButton(button, timeframe) {
 }
 
 ["daily", "weekly", "monthly"].forEach((timeframe) => {
-  addEventListenerToButton(document.querySelector(`.${timeframe}-btn`), timeframe);
+  addEventListenerToButton(
+    document.querySelector(`.${timeframe}-btn`),
+    timeframe
+  );
+});
+
+ellipsisBtns.forEach((btn) => {
+  const deleteBtn = btn.parentElement.querySelector(".delete-btn");
+  btn.addEventListener("click", () => {
+    deleteBtn.classList.toggle("hidden");
+  });
+
+  deleteBtn.addEventListener("click", () => {
+    deleteBtn.closest(".card").remove();
+  });
 });
