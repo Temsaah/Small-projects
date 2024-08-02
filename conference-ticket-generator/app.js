feff3549450cfd55758cd6ef23ce3b086d6687df
Temsaah <Seif.Temsaah@gmail.com>
2024-08-02 10:20:34 +0300

    Handle uploaded image and add display generated ticket on user page

--- conference-ticket-generator/app.js
+++ conference-ticket-generator/app.js
@@ -1,10 +1,12 @@
 const uploadLabel = document.querySelector(".upload-label");
 const imgInput = document.querySelector("#img");
+const uploadBox = document.querySelector(".upload-box");
 const uploadInfo = document.querySelector(".upload-info");
+
 console.log("🚀 ~ uploadInfo:", uploadInfo);
 const uploadStatus = document.querySelector(".upload-status");
 const initialUploadInfo = uploadInfo.innerHTML;
-console.log("🚀 ~ imgInput:", imgInput);
+let picSrc;
 
 uploadLabel.addEventListener("keydown", (e) => {
   if (e.key === " " || e.key === "Enter") {
@@ -35,11 +37,12 @@ imgInput.addEventListener("change", (e) => {
     const reader = new FileReader();
 
     reader.onload = (e) => {
+      picSrc = e.target.result;
       const imgPreview = document.createElement("img");
       imgPreview.src = e.target.result;
       imgPreview.alt = "Uploaded Avatar";
       imgPreview.className =
-        "w-16 h-16 rounded-lg border border-gray-400 object-cover";
+        "w-14 h-14 rounded-lg border border-gray-400 object-cover";
 
       uploadInfo.innerHTML = `<div
                   class="upload-icon border border-neutral-300/15 bg-neutral-300/15 w-fit rounded-xl"
@@ -49,13 +52,13 @@ imgInput.addEventListener("change", (e) => {
                 <div class="btns">
                   <button
                     type="button"
-                    class="text-gray-300/90 bg-neutral-700/50 hover:bg-neutral-700 transition-colors py-1 px-2 rounded-md text-xs underline underline-offset-2"
+                    class="remove-btn text-gray-300/90 bg-neutral-700/50 hover:bg-neutral-700 transition-colors py-1 px-2 rounded-md text-xs underline underline-offset-2"
                   >
                     Remove image
                   </button>
                   <button
                     type="button"
-                    class="text-gray-300/90 bg-neutral-700/50 hover:bg-neutral-700 transition-colors py-1 px-2 rounded-md text-xs"
+                    class="change-btn text-gray-300/90 bg-neutral-700/50 hover:bg-neutral-700 transition-colors py-1 px-2 rounded-md text-xs"
                   >
                     Change image
                   </button>
@@ -63,5 +66,14 @@ imgInput.addEventListener("change", (e) => {
     };
 
     reader.readAsDataURL(file);
+  }
+});
+
+uploadBox.addEventListener("click", (e) => {
+  if (e.target.className.includes("remove-btn")) {
+    e.preventDefault();
+    picSrc = "";
+    uploadInfo.innerHTML = initialUploadInfo;
+    imgInput.value = "";
   }
 });
